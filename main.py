c37fde2f727acacae8c042ee9546f320484a95b0
Nic0ro <willarossi1@gmail.com>
2024-10-23 15:26:36 -0400

    ahhh

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 from flask import Flask, render_template
 from flask.views import View
+import csv
 
-class basic(View):
-    def __init__(self):
-        
+main=Flask(__name__)
+
+class Basic(View):
+    def __init__(self,name,template_name):
+        self.name=name
+        self.template_name = template_name
+        self.nav_items=[
+            {'name':'Home','url':'/'},
+            {'name':'Teams','url':'/teams'},
+            {'name':'Sign Up','url':'/signup'},
+            {'name':'About Us','url':'/aboutus'},
+            {'name':'e-Mitre','url':'/emitre'}
+        ]
+    
+    def dispatch_request(self):
+        return render_template(
+            self.template_name,
+            nav_items=self.nav_items,
+            name=self.name
+        )
+
+templates=[]
+
+with open('template.csv', mode='r')as file:
+    csvFile = csv.DictReader(file)
+    for line in csvFile:
+        templates.append(line)
+
+for temp in templates:
+    main.add_url_rule(temp['route'], view_func = Basic.as_view(temp['name'],temp['name'], temp['tempName']))
+
+if __name__ =='__main__':
+    main.run(debug=True)
